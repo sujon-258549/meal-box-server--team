2ed94899802c6e3cf1b71717ef23a6ee17f95315
kmdripon1991 <kmdripon1991@gmail.com>
2025-04-25 21:17:09 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      'http://localhost:3000',
+      'https://meal-box-team-client.vercel.app',
       // 'https://stationary-client-psi.vercel.app',
     ],
     credentials: true,
